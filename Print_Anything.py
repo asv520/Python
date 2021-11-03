ef3e4abebcab0c9b6af9497cdc27a428a7f43601
asv520 <iamasv520@gmail.com>
2021-11-03 22:37:40 +0530

    minor fix

--- Print_Anything.py
+++ Print_Anything.py
@@ -1,13 +1,13 @@
-#print single value
+# print single value
 language = "Python"
 print(language)
 
-#print multiple values
+# print multiple values
 language = "Python"
 version = 3.9
 print(language, version)
 
-#combine the values with a text
+# combine the values with a text
 language = "Python"
 version = 3.9
 print("The language is", language, "and the version is", version)
