77e0e047445a34d04f522cad644131cf5998b040
asv520 <iamasv520@gmail.com>
2021-11-05 22:54:58 +0530

    Add Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,25 @@
+# A dictionary is a collection of items that are changeable
+# Items are unordered
+# Each item is in the form of key-value pair
+# No duplicates are allowed
+
+Dictionary_1 = {"Name": "Gordon Freeman"}
+
+# use keys() method to get the keys in a dictionary
+print(Dictionary_1.keys())
+
+# use the values() method to get the values in the dictionary
+print(Dictionary_1.values())
+
+# other methods in a dictionary
+# clear()	Removes all the elements from the dictionary
+# copy()	Returns a copy of the dictionary
+# fromkeys()	Returns a dictionary with the specified keys and value
+# get()	Returns the value of the specified key
+# items()	Returns a list containing a tuple for each key value pair
+# keys()	Returns a list containing the dictionary's keys
+# pop()	Removes the element with the specified key
+# popitem()	Removes the last inserted key-value pair
+# setdefault() Returns the value of the specified key. If the key does not exist: insert the key, with specified value
+# update()	Updates the dictionary with the specified key-value pairs
+# values()	Returns a list of all the values in the dictionary
