906ec88f07309bb8765380bb3c195b29a0c43f51
asv520 <iamasv520@gmail.com>
2021-11-06 11:00:52 +0530

    Add Lambda Functions

---
+++ LambdaFunctions.py
@@ -0,0 +1,22 @@
+# Python Lambda Functions are anonymous function means that the function is without a name.
+# As we already know that the def keyword is used to define a normal function in Python.
+# Similarly, the lambda keyword is used to define an anonymous function in Python.
+# This function can have any number of arguments but ONLY ONE expression, which is evaluated and returned.
+# One is free to use lambda functions wherever function objects are required.
+# You need to keep in your knowledge that lambda functions are syntactically restricted to a single expression.
+# It has various uses in particular fields of programming besides other types of expressions in functions.
+
+# Python code to illustrate cube of a number
+# showing difference between def() and lambda().
+def cube(y):
+    return y * y * y
+
+
+lambda_cube = lambda y: y * y * y
+
+# using the normally
+# defined function
+print(cube(5))
+
+# using the lambda function
+print(lambda_cube(5))
